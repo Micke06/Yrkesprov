23eb7c6f4a58dd3aa01636958859e1f8b337045b
Micke06 <117626367+Micke06@users.noreply.github.com>
2023-02-13 13:14:18 +0200

    Update yrkesprov1.ps1

--- yrkesprov1.ps1
+++ yrkesprov1.ps1
@@ -1,8 +1,8 @@
-Webrequest = Invoke-WebRequest "https://www.avoindata.fi/data/dataset/d203038a-fa46-40b8-80b4-33479bf64412/resource/a71df935-054d-4e83-8e07-e2e15f6224ed/download/ostolaskudata-2021-porvoon-kaupunki.csv" -UseBasicParsing -OutFile "C:\temp\Räkningar.csv"
+$webrequest = Invoke-WebRequest "https://www.avoindata.fi/data/dataset/d203038a-fa46-40b8-80b4-33479bf64412/resource/a71df935-054d-4e83-8e07-e2e15f6224ed/download/ostolaskudata-2021-porvoon-kaupunki.csv" -UseBasicParsing -OutFile "/home/kenny/Räkningar.csv"
 
-$Csv = Import-Csv -path home\kenny\Räkningar.csv -Delimiter ';' -Encoding utf7
+$Csv = Import-Csv -path /home/kenny/Räkningar.csv -Delimiter ';' -Encoding utf8
 
-$database ="C:\temp\Yrkesprov1.db"
+$database ="/home/kenny/Yrkesprov1.db"
 $query = "CREATE TABLE PORVOO (INTEGER PRIMARY KEY,
 kustannus_id INTEGER,
 kustannus_name TEXT,
@@ -16,3 +16,4 @@ Invoke-sqlitequery -query $query -Datasource $database
 $tilldatabasen | ForEach-Object {
 
 $query = "INSERT INTO Yrkesprov1
+
