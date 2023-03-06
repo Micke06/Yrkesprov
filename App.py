70e4dcafcb537696c38ca40ba672dd0039ec8898
Micke06 <117626367+Micke06@users.noreply.github.com>
2023-03-06 13:20:03 +0200

    Update App.py

--- App.py
+++ App.py
@@ -11,30 +11,37 @@ def home():
         ("01-03-2020", 1908),
         ("01-04-2020", 896),
         ("01-05-2020", 755),
-        ("01-06-2020", 355),
-        ("01-07-2020", 155),
-        ("01-08-2020", 145),
-        ("01-09-2020", 725),
+      #  ("01-06-2020", 355),
+      #  ("01-07-2020", 155),
+      #  ("01-08-2020", 145),
+      #  ("01-09-2020", 725),
     ]
-    colors = ['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)','rgb(25, 135, 69)', 'rgb(139, 100, 100)','rgb(10, 255, 20)', 'rgb(255,255, 86)','rgb(255, 178, 90)','rgb(155, 150, 69)']
-   
-   
- 
+    #colors = ['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)','rgb(25, 135, 69)', 'rgb(139, 100, 100)','rgb(10, 255, 20)', 'rgb(255,255, 86)','rgb(255, 178, 90)','rgb(155, 150, 69)']
+    con = sqlite3.connect("Yrkesprov1.db")
+    cur = con.cursor()
+    
+    for row in cur.execute('SELECT * FROM COLORS;'):print(row)
+
+    con.close()
+
     
     
    
     values = []
     labels = []
+    colors = []
     
     
     
     for row in data:
-        values.append(row[1])
-        labels.append(row[0])
-        
+         values.append(row[1])
+         labels.append(row[0])
+         colors.append(row[0])
+         
         
     #return data
-    return render_template("graph.html", labels=labels, values=values, colors=colors )
+    return render_template("graph.html", labels=labels, values=values, colors=colors)
+   # return(values)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
