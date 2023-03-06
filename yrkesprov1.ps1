07dd1f967fe3198e33eef5ec15ba9c3e2782a932
Micke06 <117626367+Micke06@users.noreply.github.com>
2023-03-06 13:19:31 +0200

    Update yrkesprov1.ps1

--- yrkesprov1.ps1
+++ yrkesprov1.ps1
@@ -1,10 +1,8 @@
-Remove-Item C:\temp\Yrkesprov1.db
+invoke-webrequest "https://www.avoindata.fi/data/dataset/d203038a-fa46-40b8-80b4-33479bf64412/resource/a71df935-054d-4e83-8e07-e2e15f6224ed/download/ostolaskudata-2021-porvoon-kaupunki.csv" -UseBasicParsing -OutFile "/home/kenny/Räkningar.csv"
 
-$webrequest = Invoke-WebRequest "https://www.avoindata.fi/data/dataset/d203038a-fa46-40b8-80b4-33479bf64412/resource/a71df935-054d-4e83-8e07-e2e15f6224ed/download/ostolaskudata-2021-porvoon-kaupunki.csv" -UseBasicParsing -OutFile "C:\temp\Räkningar.csv"
+$Csv = Import-Csv -path /home/kenny/Räkningar.csv -Delimiter ';' -Encoding utf7
 
-$Csv = Import-Csv -path C:\temp\Räkningar.csv -Delimiter ';' -Encoding utf7
-
-$database ="C:\temp\Yrkesprov1.db"
+$database ="/home/kenny/Yrkesprov1.db"
 $query = "CREATE TABLE PORVOO (INTEGER PRIMARY KEY,
 kustannus_id INTEGER,
 kustannus_name TEXT,
@@ -15,32 +13,49 @@ rondo_id INTEGER);"
 
 Invoke-sqlitequery -query $query -Datasource $database
 
-$Counter = 0
 
 
 
 $Csv | ForEach-Object {
-$Counter = $Counter + 1
-$Counter
-                     $tosite_number = $_.Tositenumero
 
-                     $Kustannus_id = $_.Kustannuspaikka
+                       $tosite_number = $_.Tositenumero
 
-                     $Kustannus_name = $_.'Kustannuspaikan nimi'
+                       $Kustannus_id = $_.Kustannuspaikka
 
-                     $tositename = $_.Selite.Replace("'" ,"")
+                       $Kustannus_name = $_.'Kustannuspaikan nimi'
 
-                     $euro_brutto = $_.'EUR, brutto'.replace(" " ,"")
+                       $tositename = $_.Selite.Replace("'" ,"")
 
-                     $rondo_id = $_.'Rondo ID'
+                       $euro_brutto = $_.'EUR, brutto'.replace(" " ,"")
 
+                       $rondo_id = $_.'Rondo ID'
    
                      $query = "INSERT INTO PORVOO (tosite_number, kustannus_id, kustannus_name, tositename, euro_brutto, rondo_id) VALUES ('$tosite_number','$Kustannus_id','$Kustannus_name','$tositename','$euro_brutto','$rondo_id');"
                      Invoke-SqliteQuery -query $query -Datasource $database
                      }
-                     
-                     
-                     
-                     
-                      sudo apt install /home/kenny/Downloads/code_1.75.1-1675893397_amd64.deb
 
+
+     invoke-sqlitequery -query "select * from PORVOO" -datasource $database       
+     
+     
+     
+     
+
+     $database ="/home/kenny/Yrkesprov1.db"
+     $query = "CREATE TABLE COLORS (INTEGER PRIMARY KEY,
+     Red INTEGER,
+     Green INTEGER,
+     Blue INTEGER);"
+
+     Invoke-sqlitequery -query $query -Datasource $database
+
+
+     ForEach-Object {
+
+     $query = "INSERT INTO COLORS (Red, Green, Blue) VALUES ('255', '99', '132'), ('54', '162', '235'), ('255', '205', '86'), ('25', '135', '69'), ('139', '100', '100');"
+     Invoke-SqliteQuery -query $query -Datasource $database
+     
+
+
+     invoke-sqlitequery -query "select * from COLORS" -datasource $database
+     }
