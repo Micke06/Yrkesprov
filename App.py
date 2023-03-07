9e4a8d4e3693e1d9870e696df95c61336ff55ac7
Micke06 <117626367+Micke06@users.noreply.github.com>
2023-03-07 10:59:05 +0200

    Update App.py

--- App.py
+++ App.py
@@ -17,10 +17,14 @@ def home():
       #  ("01-09-2020", 725),
     ]
     #colors = ['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)','rgb(25, 135, 69)', 'rgb(139, 100, 100)','rgb(10, 255, 20)', 'rgb(255,255, 86)','rgb(255, 178, 90)','rgb(155, 150, 69)']
+    #with open ('Yrkesprov1.db', encoding="latin1") as f:
+     
+    
     con = sqlite3.connect("Yrkesprov1.db")
     cur = con.cursor()
-    
-    for row in cur.execute('SELECT * FROM COLORS;'):print(row)
+    #Måste ju sparas som en variabel
+    cur.execute('SELECT HEX FROM colors;')
+    rows = cur.fetchall()
 
     con.close()
 
@@ -30,18 +34,30 @@ def home():
     values = []
     labels = []
     colors = []
+   
     
     
     
     for row in data:
          values.append(row[1])
          labels.append(row[0])
-         colors.append(row[0])
+
+    for row in rows:
+         colors.append("#" + row[0])
          
+
+   # for row in rows:
+        # colors.append(row[1])
+        # colors.append(row[2]) 
+        # colors.append(row[3]) 
+        
+
+    print(rows)
+    #colors = "Magic Micke"           
         
     #return data
     return render_template("graph.html", labels=labels, values=values, colors=colors)
-   # return(values)
+    #return(values)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
