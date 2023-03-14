4a67d42d60de16643c16708237cf78ac9a464efc
Micke06 <117626367+Micke06@users.noreply.github.com>
2023-03-14 11:28:03 +0200

    Update App.py

--- App.py
+++ App.py
@@ -4,25 +4,33 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    data = [
-        ("01-01-2020", 1597),
-        ("01-02-2020", 1456),
-        ("01-03-2020", 1908),
-        ("01-04-2020", 896),
-        ("01-05-2020", 755),
+def home():                                                                                                                                                  
+ 
+    con = sqlite3.connect("Yrkesprov1.db")
+    cur = con.cursor()
+ 
+    cur.execute('SELECT kustannus_id, kustannus_name, ROUND(sum(euro_brutto)/1000000, 1) FROM porvoo GROUP BY kustannus_id ORDER BY sum(euro_brutto) DESC LIMIT 5;')
+    data = cur.fetchall()
+
+    con.close()
+   # data = [
+        #("01-01-2020", 1597),
+        #("01-02-2020", 1456),
+       # ("01-03-2020", 1908),
+       # ("01-04-2020", 896),
+      #  ("01-05-2020", 755),
+
       #  ("01-06-2020", 355),
       #  ("01-07-2020", 155),
       #  ("01-08-2020", 145),
       #  ("01-09-2020", 725),
-    ]
+   # ]
     #colors = ['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)','rgb(25, 135, 69)', 'rgb(139, 100, 100)','rgb(10, 255, 20)', 'rgb(255,255, 86)','rgb(255, 178, 90)','rgb(155, 150, 69)']
     #with open ('Yrkesprov1.db', encoding="latin1") as f:
      
     
     con = sqlite3.connect("Yrkesprov1.db")
     cur = con.cursor()
-    #Måste ju sparas som en variabel
     cur.execute('SELECT HEX FROM colors;')
     rows = cur.fetchall()
 
@@ -30,7 +38,6 @@ def home():
 
     
     
-   
     values = []
     labels = []
     colors = []
@@ -39,11 +46,12 @@ def home():
     
     
     for row in data:
-         values.append(row[1])
-         labels.append(row[0])
+         values.append(row[2])
+         labels.append(row[1])
 
     for row in rows:
          colors.append("#" + row[0])
+         
          
 
    # for row in rows:
@@ -52,13 +60,15 @@ def home():
         # colors.append(row[3]) 
         
 
-    print(rows)
+    print(data)
     #colors = "Magic Micke"           
         
-    #return data
     return render_template("graph.html", labels=labels, values=values, colors=colors)
     #return(values)
 
 if __name__ == "__main__":
+
     app.run(port=8000, debug=True)
     
+
+ #cur.execute('SELECT "kustannus_name", "euro_brutto" FROM PORVOO ORDER BY "euro_brutto" DESC LIMIT 5')
